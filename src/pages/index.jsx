bbdb5422ec53b117b490c99c27dc0066d977f82c
Andy Jeffrey <andy.jeffrey@intel.com>
2022-10-24 16:33:10 -0700

    added app composite images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,17 +6,12 @@ import clsx from 'clsx'
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
-import {
-  TwitterIcon,
-  InstagramIcon,
-  GitHubIcon,
-  LinkedInIcon,
-} from '@/components/SocialIcons'
-import image1 from '@/images/photos/image-1.jpg'
-import image2 from '@/images/photos/image-2.jpg'
-import image3 from '@/images/photos/image-3.jpg'
-import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/photos/image-5.jpg'
+import { TwitterIcon, GitHubIcon, LinkedInIcon } from '@/components/SocialIcons'
+import image1 from '@/images/photos/treasury-suite/composite-censored.jpg'
+import image2 from '@/images/photos/matt/composite-censored.jpg'
+import image3 from '@/images/photos/placeholder.jpg'
+import image4 from '@/images/photos/placeholder.jpg'
+import image5 from '@/images/photos/placeholder.jpg'
 import logoAirbnb from '@/images/logos/airbnb.svg'
 import logoFacebook from '@/images/logos/facebook.svg'
 import logoPlanetaria from '@/images/logos/planetaria.svg'
@@ -101,7 +96,7 @@ function Article({ article }) {
 
 function SocialLink({ icon: Icon, ...props }) {
   return (
-    <Link className="group -m-1 p-1" {...props}>
+    <Link className="group -m-1 p-1" {...props} target="_blank">
       <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
     </Link>
   )
@@ -250,43 +245,46 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
-        </title>
+        <title>Andy Jeffrey - code monkey dude</title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Andy, a Cloud Application Development Engineer at Intel
+          Corporation where I have over 21 years’ experience in creating
+          intuitive line-of-business applications of all shapes and sizes.
+          I’ve built many either flying solo or by leading a team of 20 using
+          a wide spectrum of technologies from open source to enterprise. I
+          have a love of learning and a yearning to constantly improve my
+          craft."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Full stack software engineer, team lead and code monkey dude.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Andy, a <b>Cloud Application Development Engineer</b> at Intel
+            Corporation where I have over 21 years’ experience in creating
+            intuitive line-of-business applications of all shapes and sizes.
+            I’ve built many either flying solo or by leading a team of 20 using
+            a wide spectrum of technologies from open source to enterprise. I
+            have a love of learning and a yearning to constantly improve my
+            craft. I have yet to write my Shakespeare and I’ll continue pounding
+            on the keyboard until I do.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
+              href="https://twitter.com/CodeMonkeyDude"
               aria-label="Follow on Twitter"
               icon={TwitterIcon}
             />
             <SocialLink
-              href="https://instagram.com"
-              aria-label="Follow on Instagram"
-              icon={InstagramIcon}
-            />
-            <SocialLink
-              href="https://github.com"
+              href="https://github.com/codemonkeydude"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/in/andy-jeffrey-b342673/"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
